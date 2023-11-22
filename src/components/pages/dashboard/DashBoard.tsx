ac034c47f68992042d595b09b20dcc9091c62576
michaelhalevy <mh7102529@gmail.com>
2023-11-22 14:07:04 +0200

    header & headertable & orders & Table

--- src/components/pages/dashboard/DashBoard.tsx
+++ src/components/pages/dashboard/DashBoard.tsx
@@ -1,15 +1,14 @@
 import {Box} from "@mui/material";
-import Filters from "./Filters";
-import Hader from "./Hader";
+import Hader from "./Header";
+import Table from "./Table";
+
 
 const DashBoard = () => {
 
   return (
-    <Box sx={{ bgcolor: '#e0f2f1', minHeight: '100vh', height: '100vh', width: '100vw', margin: '0', position: 'absolute' }}>
+    <Box sx={{ bgcolor: '#e0f2f1', minHeight: '100vh', width: '100vw', margin: '0', position: 'absolute' }}>
       <Hader />
-      <Filters />
-      <hr color="#009688" style={{ width: '74em' }} />
-      
+      <Table />
     </Box>
   )
 }
