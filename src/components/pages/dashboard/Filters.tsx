715e46a60590bb44cbd7f64c94bfe76b1ba0484b
michaelhalevy <mh7102529@gmail.com>
2023-11-22 10:10:54 +0200

    change colors

--- src/components/pages/dashboard/Filters.tsx
+++ src/components/pages/dashboard/Filters.tsx
@@ -7,43 +7,43 @@ export default function Filters() {
                 <Box sx={{ display: 'flex', height:'10em' }}>
                     <Box sx={{ margin: '8px' }}>
                         <Typography variant='h6'>price:</Typography>
-                        <Slider aria-label="Temperature" defaultValue={30} valueLabelDisplay="auto" step={50} marks min={50} max={300} sx={{ width: '100px', color: '#8d6e63',marginRight:'5px' }} />
+                        <Slider aria-label="Temperature" defaultValue={30} valueLabelDisplay="auto" step={50} marks min={50} max={300} sx={{ width: '100px', color: '#009688',marginRight:'5px' }} />
                     </Box>
-                    <div style={{ height: '90px', width: '0.5px', backgroundColor: '#795548', marginTop: '8px' }}></div>
+                    <div style={{ height: '90px', width: '0.5px', backgroundColor: '#009688', marginTop: '8px' }}></div>
                 </Box>
                 <Box sx={{ display: 'flex', height:'10em' }}>
                     <Box sx={{ margin: '8px' }}>
                         <Typography variant='h6'>user id:</Typography>
-                        <TextField id="filled-basic" label="ID" variant="filled" sx={{ color: '#a1887f' }} />
+                        <TextField  label="ID" variant="filled" />
                     </Box>
-                    <div style={{ height: '90px', width: '0.5px', backgroundColor: '#795548', marginTop: '8px' }}></div>
+                    <div style={{ height: '90px', width: '0.5px', backgroundColor: '#009688', marginTop: '8px' }}></div>
                 </Box>
                 <Box sx={{ display: 'flex', height:'10em' }}>
                     <Box sx={{ margin: '8px' }}>
                         <Typography variant='h6'>status:</Typography>
                         <FormGroup sx={{ display: 'flex', flexDirection: 'row' }}>
-                            <FormControlLabel control={<Checkbox defaultChecked sx={{ color: '#795548' }} />} label="Cancelled" />
-                            <FormControlLabel control={<Checkbox defaultChecked sx={{ color: '#795548' }} />} label="Accepted" />
-                            <FormControlLabel control={<Checkbox defaultChecked sx={{ color: '#795548' }} />} label="Sent" />
+                            <FormControlLabel control={<Checkbox defaultChecked sx={{ color: '#009688' }} />} label="Cancelled" />
+                            <FormControlLabel control={<Checkbox defaultChecked sx={{ color: '#009688' }} />} label="Accepted" />
+                            <FormControlLabel control={<Checkbox defaultChecked sx={{ color: '#009688' }} />} label="Sent" />
                         </FormGroup>
                     </Box>
-                    <div style={{ height: '90px', width: '0.5px', backgroundColor: '#795548', marginTop: '8px' }}></div>
+                    <div style={{ height: '90px', width: '0.5px', backgroundColor: '#009688', marginTop: '8px' }}></div>
                 </Box>
                 <Box sx={{ display: 'flex', height:'10em' }}>
                     <Box sx={{ margin: '8px' }}>
                         <Typography variant='h6'>Delivery type:</Typography>
                         <FormGroup sx={{ display: 'flex', flexDirection: 'row' }}>
-                            <FormControlLabel control={<Checkbox defaultChecked sx={{ color: '#795548' }} />} label="Regular" />
-                            <FormControlLabel control={<Checkbox defaultChecked sx={{ color: '#795548' }} />} label="Express" />
-                            <FormControlLabel control={<Checkbox defaultChecked sx={{ color: '#795548' }} />} label="PickUp" />
+                            <FormControlLabel control={<Checkbox defaultChecked sx={{ color: '#009688' }} />} label="Regular" />
+                            <FormControlLabel control={<Checkbox defaultChecked sx={{ color: '#009688' }} />} label="Express" />
+                            <FormControlLabel control={<Checkbox defaultChecked sx={{ color: '#009688' }} />} label="PickUp" />
                         </FormGroup>
                     </Box>
-                    <div style={{ height: '90px', width: '0.5px', backgroundColor: '#795548', marginTop: '8px' }}></div>
+                    <div style={{ height: '90px', width: '0.5px', backgroundColor: '#009688', marginTop: '8px' }}></div>
                 </Box>
                 <Box sx={{ display: 'flex', height:'10em' }}>
                     <Box sx={{ margin: '8px' }}>
                         <Typography variant='h6'>Date:</Typography>
-                        <input type="date" style={{backgroundColor:'#bcaaa4', border:'none', borderRadius:'1em', height:'2em', width:'10em'}}></input>
+                        <input type="date" style={{backgroundColor:'#009688', border:'none', borderRadius:'1em', height:'2em', width:'10em'}}></input>
                     </Box>
                 </Box>
             </Box>
