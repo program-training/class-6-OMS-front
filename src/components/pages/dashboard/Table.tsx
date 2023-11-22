a781f75d81ce0d006ca5a75265a32f2fb16cd8fe
michaelhalevy <mh7102529@gmail.com>
2023-11-22 14:07:04 +0200

    header & headertable & orders & Table

--- src/components/pages/dashboard/Table.tsx
+++ src/components/pages/dashboard/Table.tsx
@@ -1,52 +1,71 @@
-import { Cancel, ExpandMore } from "@mui/icons-material"
-import { Box, Button, Collapse, Typography } from "@mui/material"
+import { Typography, Box, Slider, TextField, FormGroup, FormControlLabel, Checkbox } from "@mui/material";
+import Orders from "./Orders";
+import TableHeader from "./TableHeader";
 
 
-function Table() {
 
 
-    const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+
+
+
+export default function Table() {
+
+
+
+
+
+
 
 
     return (
-        <Box sx={{ height: '100vh', minHeight: '80vh', width: '100vw', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-            <Box sx={{ height: '3em', display: 'flex', justifyContent: "space-around", width: '75em' }}>
-                <Typography variant="h5">Id</Typography>
-                <Typography variant="h5">Price</Typography>
-                <Typography variant="h5">Date</Typography>
-                <Typography variant="h5">Delivery</Typography>
-                <Typography variant="h5">Status</Typography>
-            </Box>
-            <Box sx={{ border: 'black solid 1px', width: '90vw', minHeight: '50vh' }}>
-                {array.map(() => (
-                    <Box sx={{ display: 'flex'}}>
-                        <Box sx={{ height: '3em', display: 'flex', justifyContent: "space-around", width: '75%' }}>
-                            <Typography variant="h6">id</Typography>
-                            <Typography variant="h6">price</Typography>
-                            <Typography variant="h6">date</Typography>
-                            <Typography variant="h6">delivery</Typography>
-                        </Box>
-                        <Box>
-                            <Button variant="outlined" sx={{ color: '#6d4c41', border: 'none' }} startIcon={<ExpandMore sx={{ color: '#6d4c41' }} />}>
-                            Details
-                            </Button>
-                            <Collapse> 
-                            </Collapse>
-                        </Box>
-                        <Box>
-                            <Button variant="outlined" sx={{ color: '#6d4c41', border: 'none' }} startIcon={<Cancel sx={{ color: '#6d4c41' }} />}>
-                                Cancel
-                            </Button>
-                        </Box>
-                        
-                        
+        <Box>
+            <Box sx={{ height: '8em', width: '100vw', position: 'relative', display: 'flex', justifyContent: 'space-around' }}>
+                <Box sx={{ display: 'flex', height: '10em' }}>
+                    <Box sx={{ margin: '8px' }}>
+                        <Typography variant='h6'>price:</Typography>
+                        <Slider aria-label="Temperature" defaultValue={30} valueLabelDisplay="auto" step={50} marks min={50} max={300} sx={{ width: '100px', color: '#009688', marginRight: '5px' }} />
                     </Box>
-                ))}
+                    <div style={{ height: '90px', width: '0.5px', backgroundColor: '#009688', marginTop: '8px' }}></div>
+                </Box>
+                <Box sx={{ display: 'flex', height: '10em' }}>
+                    <Box sx={{ margin: '8px' }}>
+                        <Typography variant='h6'>user id:</Typography>
+                        <TextField label="ID" variant="filled" />
+                    </Box>
+                    <div style={{ height: '90px', width: '0.5px', backgroundColor: '#009688', marginTop: '8px' }}></div>
+                </Box>
+                <Box sx={{ display: 'flex', height: '10em' }}>
+                    <Box sx={{ margin: '8px' }}>
+                        <Typography variant='h6'>status:</Typography>
+                        <FormGroup sx={{ display: 'flex', flexDirection: 'row' }}>
+                            <FormControlLabel control={<Checkbox defaultChecked sx={{ color: '#009688' }} />} label="Cancelled" />
+                            <FormControlLabel control={<Checkbox defaultChecked sx={{ color: '#009688' }} />} label="Accepted" />
+                            <FormControlLabel control={<Checkbox defaultChecked sx={{ color: '#009688' }} />} label="Sent" />
+                        </FormGroup>
+                    </Box>
+                    <div style={{ height: '90px', width: '0.5px', backgroundColor: '#009688', marginTop: '8px' }}></div>
+                </Box>
+                <Box sx={{ display: 'flex', height: '10em' }}>
+                    <Box sx={{ margin: '8px' }}>
+                        <Typography variant='h6'>Delivery type:</Typography>
+                        <FormGroup sx={{ display: 'flex', flexDirection: 'row' }}>
+                            <FormControlLabel control={<Checkbox defaultChecked sx={{ color: '#009688' }} />} label="Regular" />
+                            <FormControlLabel control={<Checkbox defaultChecked sx={{ color: '#009688' }} />} label="Express" />
+                            <FormControlLabel control={<Checkbox defaultChecked sx={{ color: '#009688' }} />} label="PickUp" />
+                        </FormGroup>
+                    </Box>
+                    <div style={{ height: '90px', width: '0.5px', backgroundColor: '#009688', marginTop: '8px' }}></div>
+                </Box>
+                <Box sx={{ display: 'flex', height: '10em' }}>
+                    <Box sx={{ margin: '8px' }}>
+                        <Typography variant='h6'>Date:</Typography>
+                        <input type="date" style={{ backgroundColor: '#009688', border: 'none', borderRadius: '1em', height: '2em', width: '10em' }}></input>
+                    </Box>
+                </Box>
             </Box>
+            <hr color="#009688" style={{ width: '74em' }} />
+            <TableHeader />
+            <Orders />
         </Box>
     )
 }
-
-export default Table
-
-
---
+++ src/components/pages/dashboard/TableHeader.tsx
@@ -0,0 +1,25 @@
+
+import { Box, Typography } from "@mui/material"
+
+
+function TableHeader() {
+
+
+    return (
+        <Box sx={{ height: '10vh', minHeight: '10vh', width: '100vw', display: 'flex', flexDirection: 'column', marginTop:'2em' }}>
+            <Box sx={{ height: '3em', display: 'flex', width: '80vw'}}>
+                <Box sx={{width:'70vw', display:'flex', marginLeft:'3em', justifyContent:'space-around'}}>
+                    <Typography variant="h5" >price</Typography>
+                    <Typography variant="h5" >date</Typography>
+                    <Typography variant="h5" >delivery</Typography>
+                    <Typography variant="h5" >status</Typography>
+                </Box>
+            </Box>
+
+        </Box>
+    )
+}
+
+export default TableHeader
+
+
