0bdfe42a8a9027896187c4b5be360cbd9e30c34c
michaelhalevy <mh7102529@gmail.com>
2023-11-22 10:10:54 +0200

    change colors

--- src/components/pages/dashboard/DashBoard.tsx
+++ src/components/pages/dashboard/DashBoard.tsx
@@ -5,10 +5,11 @@ import Header from "./Header"
 const DashBoard = () => {
 
   return (
-    <Box sx={{ bgcolor: '#d7ccc8', minHeight: '100vh', height: '100vh', width: '100vw', margin: '0', position: 'absolute' }}>
+    <Box sx={{ bgcolor: '#e0f2f1', minHeight: '100vh', height: '100vh', width: '100vw', margin: '0', position: 'absolute' }}>
       <Header />
       <Filters />
-      <hr color="#4e342e" style={{ width: '74em' }} />
+      <hr color="#009688" style={{ width: '74em' }} />
+      
     </Box>
   )
 }
