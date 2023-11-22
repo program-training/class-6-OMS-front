e4402ea7b9140d3a631d7d79332fc56b42d5c510
Davi Sopher <dssopher@gmail.com>
2023-11-22 11:30:21 +0200

    header, index fixes'

--- src/components/pages/dashboard/DashBoard.tsx
+++ src/components/pages/dashboard/DashBoard.tsx
@@ -1,15 +1,15 @@
 import {Box} from "@mui/material";
 import Filters from "./Filters";
 import Header from "./Header"
+import Header from "./Header"
 
 const DashBoard = () => {
 
   return (
-    <Box sx={{ bgcolor: '#e0f2f1', minHeight: '100vh', height: '100vh', width: '100vw', margin: '0', position: 'absolute' }}>
+    <Box sx={{ bgcolor: '#d7ccc8', minHeight: '100vh', height: '100vh', width: '100vw', margin: '0', position: 'absolute' }}>
       <Header />
       <Filters />
-      <hr color="#009688" style={{ width: '74em' }} />
-      
+      <hr color="#4e342e" style={{ width: '74em' }} />
     </Box>
   )
 }
