d1008eed193213697743492afa0b1a0ad7cbb453
Davi Sopher <dssopher@gmail.com>
2023-11-23 14:32:48 +0200

    font change

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import MyRouter from "./components/myrouter/MyRouter"
+import { createTheme, ThemeProvider } from "@mui/material"
 
+const theme = createTheme({
+  typography: {
+    fontFamily: 'Barlow, sans-serif'
+  }
+})
 
 function App() {
   
 
   return (
     <>
+    <ThemeProvider theme={theme}> 
     <MyRouter/>
+    </ThemeProvider>
     </>
   )
 }
